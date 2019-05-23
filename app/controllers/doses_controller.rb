6b4a14bd34c1121521aa0aaf437405721ea22306
Alex R <jpalre@outlook.com>
2019-05-24 01:07:22 +0900

    style was changed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,23 +1,26 @@
 class DosesController < ApplicationController
 
-  def index
-  end
-
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@dose.cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render cocktail_path(@cocktail)
     end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail_id, presence: true
-  validates :ingredient_id, presence: true
   validates :description, presence: true
-  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
